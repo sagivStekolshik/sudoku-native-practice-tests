10852c935993e23486ef99da69b33e6b8835084d
sagivStekolshik <sagivste@gmail.com>
2019-09-17 22:48:19 +0300

    add commentgit config --global user.name

--- src/__test__/unitTests/validityChecks.test.js
+++ src/__test__/unitTests/validityChecks.test.js
@@ -1,35 +1,36 @@
 import { isValid } from "../../reducers/validityChecks";
 
-describe('validityChecks', () => {
-    const sudokuRoot = 2
-    const grid = []
-    
-    beforeAll(() => {
-      /* Runs before all tests */
-    })
-    
-    afterAll(() => {
-      /* Runs after all tests */
-    })
-   
-    beforeEach(() => {
-        for (let row = 0; row < Math.pow(sudokuRoot, 2); row++) {
-            grid[row] = [];
-          for (let col = 0; col < Math.pow(sudokuRoot, 2); col++) {
-            grid[row][col] = "";
-          }
-        }
-    })
-    afterEach(() => {
-      /* Runs after each test */
-    })
-  
-    test('different values on 0,0 and 0,1', () => {
-      grid[0][0]= "1"
-      expect(isValid(grid)).toBeTruthy()
-    })
+describe("validityChecks", () => {
+  const sudokuRoot = 2;
+  const grid = [];
 
-    test('same values on 0,0 and 0,1', () => {
-        expect(isValid(grid)).toBeFalsy()
-      })
-  })
+  beforeAll(() => {
+    /* Runs before all tests */
+  });
+
+  afterAll(() => {
+    /* Runs after all tests */
+  });
+
+  // before each test clean the grid
+  beforeEach(() => {
+    for (let row = 0; row < Math.pow(sudokuRoot, 2); row++) {
+      grid[row] = [];
+      for (let col = 0; col < Math.pow(sudokuRoot, 2); col++) {
+        grid[row][col] = "";
+      }
+    }
+  });
+  afterEach(() => {
+    /* Runs after each test */
+  });
+
+  test("different values on 0,0 and 0,1", () => {
+    grid[0][0] = "1";
+    expect(isValid(grid)).toBeTruthy();
+  });
+
+  test("same values on 0,0 and 0,1", () => {
+    expect(isValid(grid)).toBeFalsy();
+  });
+});
